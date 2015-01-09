9354b9e6b44f111187c509c3c3da2d581872ae77
PhilipHuang2 <philiphuang2@gmail.com>
2015-01-09 11:27:23 -0800

    done

--- build-tmp/source/PalindromeChecker.java
+++ build-tmp/source/PalindromeChecker.java
@@ -33,7 +33,28 @@ public void setup()
 public boolean palindrome(String word)
 {
   //your code here
+  String delims = "[ .,?!']+";
+  String[] tokens = word.split(delims);
+  String trueSentence = "";
+  for(int a = 0; a < tokens.length; a++)
+  {
+   trueSentence = trueSentence + tokens[a];
+  }
+  trueSentence = trueSentence.toLowerCase();
+  // now have a string called trueSentence that can be reversed
+  
+  String rever = "";
+  for(int a = 0; a < trueSentence.length(); a++)
+  {
+    rever = rever + trueSentence.substring(trueSentence.length()-a-1,trueSentence.length()-a); 
+  }
+  if(trueSentence.equals(rever))
+  {return true;}
+  else
   return false;
+
+  //Character.isLetter()
+
 }
 
   static public void main(String[] passedArgs) {
